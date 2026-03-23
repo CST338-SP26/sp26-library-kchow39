245b1ed971b37ae7ca6db9dcb93367520d6d061c
kchow39 <kachow@csumb.edu>
2026-03-23 14:42:39 -0700

    started Reader.java No methods yet defined Note: warnings due to Reader not being finished

---
+++ src/main/java/Reader.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Kaitlyn Chow
+ * This is a POJO that represents the Reader
+ * used in the larger Library project
+ */
+public class Reader {
+    public static final int CARD_NUMBER_ = 0;
+    public static final int NAME_ = 1;
+    public static final int PHONE_ = 2;
+    public static final int BOOK_COUNT_ = 3;
+    public static final int BOOK_START_ = 4;
+
+    private int cardNumber;
+    private String name;
+    private String phone;
+    private List<Book> books;
+
+    public Reader(int cardNumber, String name, String phone) {
+        this.cardNumber = cardNumber;
+        this.name = name;
+        this.phone = phone;
+        books = new ArrayList<>();
+    }
+
+}
