0f0508864a896d53825b9e0c6a60bd0f3cd76f10
kchow39 <kachow@csumb.edu>
2026-03-23 14:54:10 -0700

    continued Reader.java completed getters, setters, toString, equals and hashCode Note: warnings due to Reader not being finished

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Kaitlyn Chow
@@ -25,4 +26,52 @@ public class Reader {
         books = new ArrayList<>();
     }
 
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Reader reader = (Reader) o;
+        return getCardNumber() == reader.getCardNumber() && Objects.equals(getName(), reader.getName()) && Objects.equals(getPhone(), reader.getPhone());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCardNumber(), getName(), getPhone());
+    }
+
+    @Override
+    public String toString() {
+        return name + " (#" + cardNumber + ") has checked out " + books.toString().replace("[", "{").replace("]", "}");
+    }
 }
