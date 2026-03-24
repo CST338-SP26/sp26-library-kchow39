bc0fd100939333033db81ecde4f8d04c21382b44
kchow39 <kachow@csumb.edu>
2026-03-23 20:43:19 -0700

    finished Reader.java and added static checks to ReaderTest.java completed other methods

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -1,3 +1,5 @@
+import Utilities.Code;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -24,6 +26,33 @@ public class Reader {
         this.name = name;
         this.phone = phone;
         books = new ArrayList<>();
+    }
+
+    public Code addBook(Book book){
+        if (hasBook(book)) {
+            return Code.BOOK_ALREADY_CHECKED_OUT_ERROR;
+        }
+        books.add(book);
+        return Code.SUCCESS;
+    }
+
+    public Code removeBook(Book book){
+        if(!hasBook(book)){
+            return Code.READER_DOESNT_HAVE_BOOK_ERROR;
+        }
+        if(books.remove(book)){
+            return Code.SUCCESS;
+        } else {
+            return Code.READER_COULD_NOT_REMOVE_BOOK_ERROR;
+        }
+    }
+
+    public boolean hasBook(Book book){
+        return books.contains(book);
+    }
+
+    public int getBookCount(){
+        return books.size();
     }
 
     public int getCardNumber() {
