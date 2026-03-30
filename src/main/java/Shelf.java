7c0dfc99a415312305a6c40a540894c4522810b0
kchow39 <kachow@csumb.edu>
2026-03-30 08:28:01 -0700

    Started Shelf.java Constructor, getters, and setters defined Note: warnings due to Shelf not being finished

--- src/main/java/Shelf.java
+++ src/main/java/Shelf.java
@@ -13,4 +13,37 @@ public class Shelf {
     private int shelfNumber;
     private String subject;
 
+    public Shelf(){
+
+    }
+
+    public Shelf(int shelfNumber, String subject) {
+        this.shelfNumber = shelfNumber;
+        this.subject = subject;
+        books = new HashMap<>();
+    }
+
+    public HashMap<Book, Integer> getBooks() {
+        return books;
+    }
+
+    public void setBooks(HashMap<Book, Integer> books) {
+        this.books = books;
+    }
+
+    public int getShelfNumber() {
+        return shelfNumber;
+    }
+
+    public void setShelfNumber(int shelfNumber) {
+        this.shelfNumber = shelfNumber;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 }
