9f8fb17c4a89bb43cc2ab81e65d6337fb5098e35
kchow39 <kachow@csumb.edu>
2026-03-30 09:20:48 -0700

    finished Shelf.java uncommented ShelfTest.java

--- src/main/java/Shelf.java
+++ src/main/java/Shelf.java
@@ -1,4 +1,7 @@
+import Utilities.Code;
+
 import java.util.HashMap;
+import java.util.Objects;
 
 /**
  * @author Kaitlyn Chow
@@ -14,14 +17,14 @@ public class Shelf {
     private String subject;
 
     public Shelf(){
-
-    }
-
-    public Shelf(int shelfNumber, String subject) {
-        this.shelfNumber = shelfNumber;
-        this.subject = subject;
         books = new HashMap<>();
     }
+
+//    public Shelf(int shelfNumber, String subject) {
+//        this.shelfNumber = shelfNumber;
+//        this.subject = subject;
+//        books = new HashMap<>();
+//    }
 
     public HashMap<Book, Integer> getBooks() {
         return books;
@@ -46,4 +49,82 @@ public class Shelf {
     public void setSubject(String subject) {
         this.subject = subject;
     }
+
+    /**
+     * gets a book count
+     * @param book as Book
+     * @return the book count otherwise returns -1 if not found
+     */
+    public int getBookCount(Book book){
+        return books.getOrDefault(book, -1);
+    }
+
+    /**
+     * adds the parameter 'book' to the HashMap of books stored on the shelf
+     * @param book as Book
+     * @return a success code if added and a subject mismatch if the book does not match the subject
+     */
+    public Code addBook(Book book){
+        if(!books.containsKey(book) && !book.getSubject().equals(subject)){
+            return Code.SHELF_SUBJECT_MISMATCH_ERROR;
+        }
+        if(books.containsKey(book)){
+            books.put(book, books.get(book)+1);
+        } else {
+            books.put(book, 1);
+        }
+        System.out.println(book + " added to shelf " + this);
+        return Code.SUCCESS;
+    }
+
+    /**
+     * removes a book from the shelf
+     * @param book as Book
+     * @return a "not in inventory error" if the book is not on the shelf
+     */
+    public Code removeBook(Book book){
+        if(!books.containsKey(book)){
+            System.out.println(book.getTitle() + "is not on shelf " + subject);
+            return Code.BOOK_NOT_IN_INVENTORY_ERROR;
+        }
+        if(books.containsKey(book) && books.get(book) == 0){
+            System.out.println("No copies of " + book.getTitle() + " remain on the shelf " + subject);
+            return Code.BOOK_NOT_IN_INVENTORY_ERROR;
+        }
+        books.put(book, books.get(book)-1);
+        System.out.println(book.getTitle() + " successfully removed from shelf " + subject);
+        return Code.SUCCESS;
+    }
+
+    public String listBooks(){
+        StringBuilder sb = new StringBuilder();
+        if(books.size() == 1){
+            sb.append(books.size()).append(" book on shelf: ").append(shelfNumber).append(" : ").append(subject).append("\n");
+        } else {
+            sb.append(books.size()).append(" books on shelf: ").append(shelfNumber).append(" : ").append(subject).append("\n");
+        }
+        for(Book b : books.keySet()){
+            sb.append(books.get(b)).append(" ").append(b).append("\n");
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Shelf shelf = (Shelf) o;
+        return getShelfNumber() == shelf.getShelfNumber() && Objects.equals(getSubject(), shelf.getSubject());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getShelfNumber(), getSubject());
+    }
+
+    @Override
+    public String toString() {
+        return shelfNumber + " : " + subject;
+    }
+
+
 }
